23ff4fe42fefbebac4e6893af4ca7fdbe2b1ee8c
sbh123 <1103597933@qq.com>
2019-08-07 10:08:14 +0800

    tmp modify

--- src/test.cc
+++ src/test.cc
@@ -5,6 +5,7 @@
 #include <thread>
 
 #include "nvm_bptree.h"
+#include "random.h"
 
 #define NODEPATH   "/pmem0/datastruct/persistent"
 #define VALUEPATH "/pmem0/datastruct/value_persistent"
@@ -18,7 +19,21 @@ int main(int argc, char *argv[]) {
 
     bt->Initial(NODEPATH, NVM_NODE_SIZE, VALUEPATH, NVM_VALUE_SIZE);
 
-    bt->PrintInfo();
+    // bt->PrintInfo();
+    function_test(100);
 
     delete bt;
+}
+
+void function_test(btree *bt, uint64_t ops) {
+    printf("******B+ tree function test start.******\n");
+    rocksdb::Random rnd_put(0xdeadbeef); 
+    for(uint64_t i; i < ops; i ++) {
+        auto key = rnd_put.Next();
+        bt->btree_insert(key, (char*) keys);
+    } 
+    printf("******Insert test finished.******\n");
+    bt->printAll();
+    bt->PrintInfo();
+
 }
