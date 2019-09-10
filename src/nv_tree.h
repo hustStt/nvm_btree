2cc85088a2acff30084452dfc4deab21570398eb
sbh123 <1103597933@qq.com>
2019-09-10 09:29:42 +0800

    tmp modify

--- src/nv_tree.h
+++ src/nv_tree.h
@@ -537,8 +537,8 @@ public:
                         if(it.second.second != OpDelete) {  // 最后一次操作不是Delete
                             if(it.first >= key1 && it.second.first != nullptr) {
                                 values.push_back(string((char *)(it.second.first), NVM_ValueSize));
+                                find_size ++;
                             }
-                            find_size ++;
                             if(find_size >= size) {
                                 return ;
                             }
