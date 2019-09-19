cf4693dd9816b9fd8faf139b7bdea7ac1f0b8e95
sbh123 <1103597933@qq.com>
2019-09-19 13:52:56 +0800

    tmp modify

--- src/nv_tree.h
+++ src/nv_tree.h
@@ -546,22 +546,18 @@ public:
         int pos = parent->binary_search(key1);
 
         LeafNode *leaf = parent->LNs[pos];
-        LeafNode tmp_leaf;
-        LeafNode *p = &tmp_leaf;
 
         while(leaf != nullptr) {
-            memcpy(p, leaf, sizeof(LeafNode));
-
             std::map<uint64_t, std::pair<void *, uint8_t>> maps;
-            for (int i = p->nElements-1; i >= 0; i--)
+            for (int i = leaf->nElements-1; i >= 0; i--)
             {
                 // if (maps.find(p->elements[i].key) == maps.end())
                 // {
                 //     maps.insert(std::make_pair(p->elements[i].key,
                 //                             std::make_pair(p->elements[i].value, p->elements[i].flag)));
                 // }
-                maps.insert(std::make_pair(p->elements[i].key,
-                                            std::make_pair(p->elements[i].value, p->elements[i].flag)));
+                maps.insert(std::make_pair(leaf->elements[i].key,
+                                            std::make_pair(leaf->elements[i].value, leaf->elements[i].flag)));
             }
 
             for (auto it : maps)
