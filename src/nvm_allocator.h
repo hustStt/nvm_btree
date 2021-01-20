af492939f1d52b577fd36f192945ab055c019b59
hustStt <1255402185@qq.com>
2021-01-21 01:01:52 +0800

    update

--- src/nvm_allocator.h
+++ src/nvm_allocator.h
@@ -13,7 +13,62 @@ const uint64_t NVMSectorSize = 256;
 const uint64_t MemReserved = (10 << 20);  // 保留 10M 空间
 const uint64_t LogSize = (200 << 20);
 
-class NVMAllocator;
+class NVMAllocator {
+public:
+    NVMAllocator(const std::string path, size_t size, uint64_t log_num) {
+        //pmemaddr_ = static_cast<char *>(pmem_map_file(path.c_str(), size, PMEM_FILE_CREATE | PMEM_FILE_EXCL, 0666, &mapped_len_, &is_pmem_));
+        //映射NVM空间到文件
+        pmemaddr_ = static_cast<char *>(pmem_map_file(path.c_str(), size, PMEM_FILE_CREATE, 0666, &mapped_len_, &is_pmem_));
+            
+        if (pmemaddr_ == NULL) {
+            printf("%s: map error, filepath %s, error: %s(%d)\n", __FUNCTION__, path.c_str(), strerror(errno), errno);
+            exit(-1);
+        } else {
+            printf("%s: map at %p \n", __FUNCTION__, pmemaddr_);
+        }
+        begin_addr = pmemaddr_ + log_num / 8;
+        capacity_ = size;
+        log_num_ = log_num;
+    }
+
+
+    ~NVMAllocator() {
+        pmem_unmap(pmemaddr_, mapped_len_);
+    }
+
+    char* getNewLog() {
+        for (uint64_t i = 0; i < log_num_ / 8; i++) {
+            if (pmemaddr_[i] == 0xFF) {
+                continue;
+            }
+            for (int j = 0; j < 8;j ++) {
+                if (((1 << j) & pmemaddr_[i]) == 0) {
+                    return begin_addr + (8 * i + j) * LogSize;
+                }
+            }
+        }
+        return nullptr;
+    }
+
+    void deleteLog(char *addr) {
+        pmem_memset_persist(addr, 0, LogSize);
+        uint64_t i = (addr - begin_addr) / LogSize;
+        pmemaddr_[i / 8] &= ~(1 << (i % 8)); 
+    }
+
+    void ResetZero() {
+        pmem_memset_persist(pmemaddr_, 0, mapped_len_);
+    }
+
+private:
+    char* pmemaddr_;
+    char* begin_addr;
+    size_t mapped_len_;
+    uint64_t capacity_;
+    uint64_t log_num_;
+    int is_pmem_;
+};
+
 
 class LogAllocator {
 private:
@@ -81,62 +136,6 @@ private:
 
 };
 
-
-class NVMAllocator {
-public:
-    NVMAllocator(const std::string path, size_t size, uint64_t log_num) {
-        //pmemaddr_ = static_cast<char *>(pmem_map_file(path.c_str(), size, PMEM_FILE_CREATE | PMEM_FILE_EXCL, 0666, &mapped_len_, &is_pmem_));
-        //映射NVM空间到文件
-        pmemaddr_ = static_cast<char *>(pmem_map_file(path.c_str(), size, PMEM_FILE_CREATE, 0666, &mapped_len_, &is_pmem_));
-            
-        if (pmemaddr_ == NULL) {
-            printf("%s: map error, filepath %s, error: %s(%d)\n", __FUNCTION__, path.c_str(), strerror(errno), errno);
-            exit(-1);
-        } else {
-            printf("%s: map at %p \n", __FUNCTION__, pmemaddr_);
-        }
-        begin_addr = pmemaddr_ + log_num / 8;
-        capacity_ = size;
-        log_num_ = log_num;
-    }
-
-
-    ~NVMAllocator() {
-        pmem_unmap(pmemaddr_, mapped_len_);
-    }
-
-    char* getNewLog() {
-        for (uint64_t i = 0; i < log_num_ / 8; i++) {
-            if (pmemaddr_[i] == 0xFF) {
-                continue;
-            }
-            for (int j = 0; j < 8;j ++) {
-                if (((1 << j) & pmemaddr_[i]) == 0) {
-                    return begin_addr + (8 * i + j) * LogSize;
-                }
-            }
-        }
-        return nullptr;
-    }
-
-    void deleteLog(char *addr) {
-        pmem_memset_persist(addr, 0, LogSize);
-        uint64_t i = (addr - begin_addr) / LogSize;
-        pmemaddr_[i / 8] &= ~(1 << (i % 8)); 
-    }
-
-    void ResetZero() {
-        pmem_memset_persist(pmemaddr_, 0, mapped_len_);
-    }
-
-private:
-    char* pmemaddr_;
-    char* begin_addr;
-    size_t mapped_len_;
-    uint64_t capacity_;
-    uint64_t log_num_;
-    int is_pmem_;
-};
 
 extern NVMAllocator *node_alloc;
 extern NVMAllocator *value_alloc;
