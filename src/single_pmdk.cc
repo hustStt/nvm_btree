6616c214dfa7fbada72d7bd57b7148a9542c8461
hustStt <1255402185@qq.com>
2021-02-22 18:12:39 +0800

    log test

--- src/single_pmdk.cc
+++ src/single_pmdk.cc
@@ -813,7 +813,7 @@ void nvmpage::linear_search_range(entry_key_t min, entry_key_t max, void **value
 void subtree::subtree_insert(btree* root, entry_key_t key, char* right) {
   if (flag) {
     // write log
-    //log_alloc->writeKv(key, right);
+    log_alloc->writeKv(-1, key, right);
     bpnode *p = dram_ptr;
 
     while(p->hdr.leftmost_ptr != NULL) {
@@ -868,7 +868,7 @@ void subtree::subtree_update(btree* root, entry_key_t key, char* right) {
 void subtree::subtree_delete(btree* root, entry_key_t key) {
   if (flag) {
     // write log
-    //log_alloc->deleteKey(-1, key);
+    log_alloc->deleteKey(-1, key);
     bpnode* p = dram_ptr;
 
     while(p->hdr.leftmost_ptr != NULL){
