bac1dd08826c1d630810289ad3dac1e1ab341291
hustStt <1255402185@qq.com>
2021-04-05 11:47:01 +0800

    hash ycsb

--- ycsb.cc
+++ ycsb.cc
@@ -68,9 +68,9 @@ public:
     }
     int Put(uint64_t key, uint64_t value) 
     {
-        char* v = NVM::data_alloc->alloc(1024);
+        char* v = (char *)NVM::data_alloc->alloc(1024);
         pmem_memcpy_persist(v,&value,1024);
-        tree_->btreeInsert(key, (char *)v);
+        tree_->btreeInsert(key, v);
         return 1;
     }
     int Get(uint64_t key, uint64_t &value)
@@ -80,9 +80,9 @@ public:
     }
     int Update(uint64_t key, uint64_t value) {
         //tree_->btreeDelete(key);
-        char* v = NVM::data_alloc->alloc(1024);
+        char* v = (char *)NVM::data_alloc->alloc(1024);
         pmem_memcpy_persist(v,&value,1024);
-        tree_->btreeUpdate(key, (char *)v);
+        tree_->btreeUpdate(key, v);
         return 1;
     }
     int Scan(uint64_t start_key, int len, std::vector<std::pair<uint64_t, uint64_t>>& results) 
